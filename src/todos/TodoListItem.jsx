233ac11d1f5348651f6c511515626cb12cff90c5
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-09-13 18:50:11 +0200

    completed styling

--- src/todos/TodoListItem.jsx
+++ src/todos/TodoListItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import classnames from 'classnames';
+
 import './TodoListItem.css';
 
 const TodoListItem = ({ todo: { text, isCompleted }, onRemovePressed, onCompletedPressed }) => {
   const handleRemove = () => onRemovePressed(text);
   const handleComplete = () => onCompletedPressed(text);
 
+  const containerClasses = classnames('todo-item-container', {
+    ['todo-item-container-completed']: isCompleted,
+  });
+
   return (
-    <div className="todo-item-container">
+    <div className={containerClasses}>
       <h3>{text}</h3>
       <div className="buttons-container">
         {isCompleted ? null : (
