a34776edc4fa9d713b302022244565b41f798a13
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-09-14 21:06:00 +0200

    combine selectors with reselect

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,7 +1,18 @@
+import { createSelector } from 'reselect';
 // const getTodos = state => state.todos;
 // const getTodosLoading = state => state.isLoading;
 
 const getTodos = state => state.todos.data;
 const getTodosLoading = state => state.todos.isLoading;
+
+// const getIncompleteTodos = createSelector(
+//     getTodos,
+//     getTodosLoading,
+//     (todos, isLoading) => isLoading ? [] : todos.filter(todo => !todo.isCompleted),
+// );
+
+const getIncompleteTodos = createSelector(getTodos, todos =>
+  todos.filter(todo => !todo.isCompleted),
+);
 
 export { getTodos, getTodosLoading };
